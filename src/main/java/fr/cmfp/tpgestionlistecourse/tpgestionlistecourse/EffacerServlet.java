cba6a72928c81fd76ed1a76246e7a9b7390c0496
M-Adrien74 <maurin.adrien.51@gmail.com>
2022-07-05 14:21:48 +0200

    update requete

--- src/main/java/fr/cmfp/tpgestionlistecourse/tpgestionlistecourse/EffacerServlet.java
+++ src/main/java/fr/cmfp/tpgestionlistecourse/tpgestionlistecourse/EffacerServlet.java
@@ -1,15 +1,30 @@
 package fr.cmfp.tpgestionlistecourse.tpgestionlistecourse;
 
+import fr.cmfp.tpgestionlistecourse.dal.ConnectionProvider;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet("/Effacer")
 public class EffacerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Connection connection = ConnectionProvider.getConnection();
+            PreparedStatement pstmt = connection.prepareStatement(
+                    "UPDATE articles SET coche = 0"
+            );
+            pstmt.executeUpdate();
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
         request.getRequestDispatcher("WEB-INF/panier.jsp").forward(request, response);
     }
 
